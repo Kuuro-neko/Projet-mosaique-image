2a09550ce91c21dce968cd86515f983b4789acaf
DupuisThibaut <tpuispuis@gmail.com>
2025-04-11 20:55:24 +0000

    changement interface choix strategie

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,14 @@
 #include <FL/Fl_Box.H>
 #include <FL/Fl_Progress.H>
 Fl_Progress* progressBar;
+Fl_Button* buttonMeanColor;
+Fl_Button* buttonVariance;
+Fl_Button* buttonSkewness ;
+Fl_Button* buttonEnergy;
+Fl_Button* buttonReuseImages ;
+Fl_Button* buttonAlignement ;
+Fl_Button* buttonKmeans ;
+Fl_Button* buttonStatMoyenne ;
 
 #include "includes/alignmentMosaic.hpp"
 #include "includes/meanFeatureMosaic.hpp"
@@ -53,7 +61,7 @@ struct MosaicParams {
     int blockSize;
     std::string imagePath;
 };
-bool MC=true,V=true,S=false,E=false,IU=false,A=false;
+bool MC=true,V=true,S=false,E=false,IU=false,A=false,K=false,SM=true;
 std::string image;
 
 void buttonParamImageMC(Fl_Widget* widget, void* data) {
@@ -107,18 +115,65 @@ void buttonParamImageIU(Fl_Widget* widget, void* data) {
     }
 }
 void buttonParamImageAlignement(Fl_Widget* widget, void* data) {
-    A=!A;
-    if(A){
-        widget->color(FL_WHITE); 
-        widget->labelcolor(FL_BLACK);
-    }else{
-        widget->color(FL_BLACK); 
-        widget->labelcolor(FL_WHITE);
-    }
+    A=true;
+    SM=false;
+    K=false;
+    buttonMeanColor->hide();
+    buttonVariance->hide();
+    buttonSkewness->hide();
+    buttonEnergy->hide();
+    buttonReuseImages->show();
+    widget->color(FL_WHITE); 
+    widget->labelcolor(FL_BLACK);
+    buttonKmeans->color(FL_BLACK);
+    buttonKmeans->labelcolor(FL_WHITE);
+    buttonKmeans->redraw();
+    buttonStatMoyenne->color(FL_BLACK);
+    buttonStatMoyenne->labelcolor(FL_WHITE);
+    buttonStatMoyenne->redraw();
+    Fl::check();
+}
+void buttonParamImageStatMoyenne(Fl_Widget* widget, void* data) {
+    SM=true;
+    A=false;
+    K=false;
+    buttonMeanColor->show();
+    buttonVariance->show();
+    buttonSkewness->show();
+    buttonEnergy->show();
+    buttonReuseImages->show();
+    widget->color(FL_WHITE); 
+    widget->labelcolor(FL_BLACK);
+    buttonKmeans->color(FL_BLACK);
+    buttonKmeans->labelcolor(FL_WHITE);
+    buttonKmeans->redraw();
+    buttonAlignement->color(FL_BLACK);
+    buttonAlignement->labelcolor(FL_WHITE);
+    buttonAlignement->redraw();
+    Fl::check();
+}
+void buttonParamImageKmeans(Fl_Widget* widget, void* data) {
+    K=true;
+    A=false;
+    SM=false;
+    buttonMeanColor->hide();
+    buttonVariance->hide();
+    buttonSkewness->hide();
+    buttonEnergy->hide();
+    buttonReuseImages->hide();
+    widget->color(FL_WHITE); 
+    widget->labelcolor(FL_BLACK);
+    buttonStatMoyenne->color(FL_BLACK);
+    buttonStatMoyenne->labelcolor(FL_WHITE);
+    buttonStatMoyenne->redraw();
+    buttonAlignement->color(FL_BLACK);
+    buttonAlignement->labelcolor(FL_WHITE);
+    buttonAlignement->redraw();
+    Fl::check();
 }
 
 
-char msg[100];
+char msg[250];
 void fonctionButtonCreerImage(Fl_Widget* widget, void* data) {
     // progressBar->draw();
     MosaicParams* param = (MosaicParams*)data;
@@ -129,7 +184,12 @@ void fonctionButtonCreerImage(Fl_Widget* widget, void* data) {
     std::map<std::string, StatisticalFeatures> meanValues = checkIfAlreadyPreProcessed(param->datasetPath);
     cv::Mat inputImage = cv::imread(image, cv::IMREAD_COLOR);
     cv::Mat mosaic;
-    if(A){
+    if(K){
+        std::map<std::string, Tamura> tamuraValues = preprocessDatasetTamura(param->datasetPath);
+        std::map<std::string, StatisticalFeatures> meanValuesTamura = checkIfAlreadyPreProcessed(param->datasetPath);
+        // mosaic=generateMosaicUsingKmeans(inputImage, param->blockSize, param->datasetPath, tamuraValues, meanValuesTamura);
+    }
+    else if(A){
         if (IU)
             mosaic=generateMosaicUsingAlignmentSingleThread(inputImage, param->blockSize, param->datasetPath, IU);
         else
@@ -231,30 +291,39 @@ int main(int argc, char** argv )
     buffText->text("Caractéristiques de l'image :");
     dispText->buffer(buffText);
     dispText->color(FL_GRAY);
-    Fl_Button* buttonMeanColor = new Fl_Button(50, 50, 125, 25, "Couleur moyenne");
+    buttonMeanColor = new Fl_Button(50, 50, 125, 25, "Couleur moyenne");
+    buttonVariance = new Fl_Button(237, 50, 125, 25, "Variance");
+    buttonSkewness = new Fl_Button(425, 50, 125, 25, "Asymétrie");
+    buttonEnergy = new Fl_Button(50, 125, 125, 25, "Energie");
+    buttonReuseImages = new Fl_Button(237, 125, 125, 25, "Image unique");
+    buttonAlignement = new Fl_Button(50, 200, 125, 25, "Alignement");
+    buttonKmeans = new Fl_Button(237, 200, 125, 25, "Kmeans");
+    buttonStatMoyenne = new Fl_Button(425, 200, 125, 25, "Stat moyenne");
     buttonMeanColor->color(FL_WHITE); 
     buttonMeanColor->labelcolor(FL_BLACK);
     buttonMeanColor->callback(buttonParamImageMC);
-    Fl_Button* buttonVariance = new Fl_Button(237, 50, 125, 25, "Variance");
+    // buttonMeanColor->hide();
     buttonVariance->color(FL_WHITE); 
     buttonVariance->labelcolor(FL_BLACK);
     buttonVariance->callback(buttonParamImageV);
-    Fl_Button* buttonSkewness = new Fl_Button(425, 50, 125, 25, "Asymétrie");
     buttonSkewness->color(FL_BLACK); 
     buttonSkewness->labelcolor(FL_WHITE);
     buttonSkewness->callback(buttonParamImageS);
-    Fl_Button* buttonEnergy = new Fl_Button(50, 125, 125, 25, "Energie");
     buttonEnergy->color(FL_BLACK); 
     buttonEnergy->labelcolor(FL_WHITE);
     buttonEnergy->callback(buttonParamImageE);
-    Fl_Button* buttonReuseImages = new Fl_Button(237, 125, 125, 25, "Image unique");
     buttonReuseImages->color(FL_BLACK); 
     buttonReuseImages->labelcolor(FL_WHITE);
     buttonReuseImages->callback(buttonParamImageIU);
-    Fl_Button* buttonAlignement = new Fl_Button(425, 125, 125, 25, "Alignement");
     buttonAlignement->color(FL_BLACK); 
     buttonAlignement->labelcolor(FL_WHITE);
     buttonAlignement->callback(buttonParamImageAlignement);
+    buttonKmeans->color(FL_BLACK); 
+    buttonKmeans->labelcolor(FL_WHITE);
+    buttonKmeans->callback(buttonParamImageKmeans);
+    buttonStatMoyenne->color(FL_WHITE); 
+    buttonStatMoyenne->labelcolor(FL_BLACK);
+    buttonStatMoyenne->callback(buttonParamImageStatMoyenne);
     Fl_Button* buttonChoisirImage = new Fl_Button(10, 300, 100, 25, "Choisir image");
     Fl_Text_Buffer *buff=new Fl_Text_Buffer();
     Fl_Text_Display *disp = new Fl_Text_Display(120, 300, 400, 50);
